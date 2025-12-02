304e198f2b69960528eb2c77f9546669044d827e
internship.faiz@bti.id <rafaizghazian@gmail.com>
2025-12-02 14:23:29 +0700

    update frontend

--- frontend/JobKing/src/main.jsx
+++ frontend/JobKing/src/main.jsx
@@ -1,10 +1,21 @@
+/*
+@application    : main javascript frontend file for JobKing application
+@description    : This file is the entry point for the React application. It sets up the React environment,
+                  including StrictMode and BrowserRouter for routing, and renders the main App component
+                  into the root DOM element.
+@author         : Rafaiz Ghazian Lusaid
+*/
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
